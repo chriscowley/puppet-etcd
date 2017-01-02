3a6af263d2162e318d4bd4a8beffe53bcc680e62
Chris Cowley <chris.cowley@groupe-regards.com>
2017-01-02 14:25:54 +0100

    Service started through systemd

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,9 +2,14 @@
 #
 class etcd::install {
   if $::etcd::package_url {
-    archive { "/tmp/etcd.tar.gz":
-      ensure => present,
-      source => $::etcd::package_url,
+    archive { '/tmp/etcd.tar.gz':
+      ensure          => present,
+      source          => $::etcd::package_url,
+      extract         => true,
+      extract_path    => '/opt/etcd',
+      extract_command => 'tar xfz %s --strip-components=1',
+      creates         => '/opt/etcd/etcd',
+
     }
   } elsif $::etcd::manage_package {
     package { $::etcd::package_name: ensure => $::etcd::ensure, }
--- manifests/service.pp
+++ manifests/service.pp
@@ -4,6 +4,14 @@
 #
 class etcd::service {
   if $::etcd::manage_service {
+    if $::etcd::package_url {
+      include ::systemd
+      systemd::service { 'etcd':
+        description => 'Etcd Key/Value store',
+        execstart   => '/opt/etcd/etcd',
+        wants       => ['network.target'],
+      }
+    }
     service { 'etcd':
       ensure => $::etcd::service_ensure,
       enable => $::etcd::service_enable,
